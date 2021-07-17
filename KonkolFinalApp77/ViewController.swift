944d85aebd581be70fea4b35e1963435d5f5725d
Chuck Konkol <ckonkol@gmail.com>
2021-07-17 09:45:02 -0500

    Initial commit

---
+++ KonkolFinalApp77/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KonkolFinalApp77
+//
+//  Created by RVCTechBusTeacher on 7/17/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
